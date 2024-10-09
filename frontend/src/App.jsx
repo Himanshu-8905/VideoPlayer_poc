18b950481ab205a901918df8cbbcd5bb821dcda3
Himanshu-8905 <kumawatbasant555@gmail.com>
2024-10-09 17:50:16 +0530

    make VideoPlayer_poc

---
+++ frontend/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import VideoPlayer from './VideoPlayer'
+import { useRef } from 'react'
+
+function App() {
+  const playerRef = useRef(null)
+  const videoLink = "http://localhost:8000/uploads/courses/b001a6c6-7a44-4f89-8258-94b4bb8e4086/index.m3u8"
+
+  const videoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: videoLink,
+        type: "application/x-mpegURL"
+      }
+    ]
+  }
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+  return (
+    <>
+      <VideoPlayer
+      options={videoPlayerOptions}
+      onReady={handlePlayerReady}
+      />
+    </>
+  )
+}
+
+export default App
